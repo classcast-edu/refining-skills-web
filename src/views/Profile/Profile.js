375c7cb215feb8f9cf11f28613bc2c0462616f6f
ayushrajdev <ayushraj@classcast.in>
2021-02-04 00:40:35 +0530

    added buttons to change institute, removed un necessary components

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,23 +1,25 @@
-import { ReactComponent as Group16681 } from "../../assets/profile/Group 1668-1.svg";
-import { ReactComponent as Group16682 } from "../../assets/profile/Group 1668-2.svg";
-import { ReactComponent as Group16683 } from "../../assets/profile/Group 1668-3.svg";
-import { ReactComponent as Group16684 } from "../../assets/profile/Group 1668-4.svg";
-import { ReactComponent as Group16685 } from "../../assets/profile/Group 1668-5.svg";
-import { ReactComponent as Group1668 } from "../../assets/profile/Group 1668.svg";
-import { ReactComponent as Group1674 } from "../../assets/profile/Group 1674.svg";
+// import { ReactComponent as Group16681 } from "../../assets/profile/Group 1668-1.svg";
+// import { ReactComponent as Group16682 } from "../../assets/profile/Group 1668-2.svg";
+// import { ReactComponent as Group16683 } from "../../assets/profile/Group 1668-3.svg";
+// import { ReactComponent as Group16684 } from "../../assets/profile/Group 1668-4.svg";
+// import { ReactComponent as Group16685 } from "../../assets/profile/Group 1668-5.svg";
+// import { ReactComponent as Group1668 } from "../../assets/profile/Group 1668.svg";
+// import { ReactComponent as Group1674 } from "../../assets/profile/Group 1674.svg";
 import { ReactComponent as Group1637 } from "../../assets/profile/Group 1637.svg";
-import { ReactComponent as EditSquare } from "../../assets/profile/EditSquare.svg";
+// import { ReactComponent as EditSquare } from "../../assets/profile/EditSquare.svg";
 import { ReactComponent as DPIcon } from "../../assets/sidenav/dp.svg";
 
 import style from "./profile.module.css";
 import ProgressBar from "components/ProgressBar/ProgressBar";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeInstitute } from "store/actions/instituteActions";
 
 const UserProfile = () => {
 	const [points, setPoints] = useState(0);
 	const instituteId = useSelector((state) => state.instituteId);
+	const displayName = useSelector((state) => state.firebase.auth.displayName);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -26,20 +28,44 @@ const UserProfile = () => {
 		};
 		fetchData();
 	}, [instituteId]);
+
+	const dispatch = useDispatch();
+
 	return (
 		<>
 			{/* <button className={style.editSquare}>
 				<EditSquare />
 			</button> */}
 			<DPIcon className={style.dp} />
-			<h2 className={`info text-align-center ${style.name}`}>Deepak Jha</h2>
-			<h2 className={`info text-align-center ${style.class}`}>Class 10</h2>
+			<h2 className={`info text-align-center ${style.name}`}>Hi, Student</h2>
+			{/* <h2 className={`info text-align-center ${style.class}`}>Class 10</h2> */}
 			<div className={style.pointsScoredContainer}>
 				<Group1637 />
 				<div>
 					<div className={style.points}>{points}</div>
 					<h2 className={style.pointsScored}>Points Scored</h2>
 				</div>
+			</div>
+
+			<div className={style.actions}>
+				<button
+					className={28 === instituteId && style.btnActive}
+					onClick={() => dispatch(changeInstitute(28))}
+				>
+					CBSE
+				</button>
+				<button
+					className={30 === instituteId && style.btnActive}
+					onClick={() => dispatch(changeInstitute(30))}
+				>
+					IGSCE
+				</button>
+				<button
+					className={31 === instituteId && style.btnActive}
+					onClick={() => dispatch(changeInstitute(31))}
+				>
+					ICSE
+				</button>
 			</div>
 		</>
 	);
