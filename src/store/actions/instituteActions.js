b6b7d9ecb4eb30b980838ed5fee6719a46f23e74
ayushrajdev <ayushraj@classcast.in>
2021-02-05 10:16:49 +0530

    set readables to empty wehn changing institute

--- src/store/actions/instituteActions.js
+++ src/store/actions/instituteActions.js
@@ -1,4 +1,5 @@
 export const changeInstitute = (payload) => (dispatch) => {
 	window.localStorage.setItem("refiningSkillsInstituteId", payload);
+	window.localStorage.setItem("readables", []);
 	return dispatch({ type: "CHANGE_INSTITUTE", payload });
 };
