2b9a18df508570acf090954272b2348d064a518a
TheDarkStrix <thedarkstrix@gmail.com>
2021-09-04 12:40:16 +0530

    Subjects: Add EVS

--- src/store/actions/practiceBySubjectActions.js
+++ src/store/actions/practiceBySubjectActions.js
@@ -4,202 +4,210 @@ import { FETCH_SUBJECTS_REQUEST } from "store/constants/practiceConstants";
 import { FETCH_SUBJECTS_SUCCESS } from "store/constants/practiceConstants";
 
 export const fetchSubjects = () => async (dispatch, getState) => {
-	const instituteId = getState().instituteId;
-	dispatch({ type: FETCH_SUBJECTS_REQUEST });
+  const instituteId = getState().instituteId;
+  dispatch({ type: FETCH_SUBJECTS_REQUEST });
 
-	try {
-		return dispatch({
-			type: FETCH_SUBJECTS_SUCCESS,
-			payload: getSubjects(instituteId),
-		});
-	} catch (error) {
-		return dispatch({ type: FETCH_SUBJECTS_FAIL });
-	}
+  try {
+    return dispatch({
+      type: FETCH_SUBJECTS_SUCCESS,
+      payload: getSubjects(instituteId),
+    });
+  } catch (error) {
+    return dispatch({ type: FETCH_SUBJECTS_FAIL });
+  }
 };
 
 const getSubjects = (instituteId) => {
-	switch (instituteId) {
-		case 31:
-			return subjectsIGCSE;
+  switch (instituteId) {
+    case 31:
+      return subjectsIGCSE;
 
-		case 30:
-			return subjectsCBSE;
+    case 30:
+      return subjectsCBSE;
 
-		default:
-			return subjectsICSE;
-	}
+    default:
+      return subjectsICSE;
+  }
 };
 const subjectsCBSE = [
-	{
-		name: "Mathematics",
-		color: "#FF78A4",
-		color2: "#FF3F7D",
-		secondaryColor: "#FF78A3",
-		color3: "#FF78A320",
-		id: 22,
-	},
-	{
-		name: "Science",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 25,
-	},
-	{
-		name: "Social Studies",
-		color: "#FF8058",
-		color2: "#FFA589",
-		secondaryColor: "#FFA88D",
-		color3: "#FFA88D20",
-		id: 35,
-	},
-	{
-		name: "English",
-		color: "#ED7685",
-		color2: "#F19DA4",
-		secondaryColor: "#F29DA4",
-		color3: "#F29DA420",
-		id: 41,
-	},
-	{
-		name: "Hindi",
-		color: "#E543FF",
-		color2: "#EF8CFF",
-		secondaryColor: "#EF90FF",
-		color3: "#FFC46A20",
-		id: 33,
-	},
+  {
+    name: "Mathematics",
+    color: "#FF78A4",
+    color2: "#FF3F7D",
+    secondaryColor: "#FF78A3",
+    color3: "#FF78A320",
+    id: 22,
+  },
+  {
+    name: "Science",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 25,
+  },
+  {
+    name: "Social Studies",
+    color: "#FF8058",
+    color2: "#FFA589",
+    secondaryColor: "#FFA88D",
+    color3: "#FFA88D20",
+    id: 35,
+  },
+  {
+    name: "English",
+    color: "#ED7685",
+    color2: "#F19DA4",
+    secondaryColor: "#F29DA4",
+    color3: "#F29DA420",
+    id: 41,
+  },
+  {
+    name: "Hindi",
+    color: "#E543FF",
+    color2: "#EF8CFF",
+    secondaryColor: "#EF90FF",
+    color3: "#FFC46A20",
+    id: 33,
+  },
 ];
 const subjectsIGCSE = [
-	{
-		name: "Mathematics",
-		color: "#FF78A4",
-		color2: "#FF3F7D",
-		secondaryColor: "#FF78A3",
-		color3: "#FF78A320",
-		id: 22,
-	},
-	{
-		name: "Physics",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 24,
-	},
-	{
-		name: "Chemistry",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 23,
-	},
-	{
-		name: "Biology",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 26,
-	},
+  {
+    name: "Mathematics",
+    color: "#FF78A4",
+    color2: "#FF3F7D",
+    secondaryColor: "#FF78A3",
+    color3: "#FF78A320",
+    id: 22,
+  },
+  {
+    name: "Physics",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 24,
+  },
+  {
+    name: "Chemistry",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 23,
+  },
+  {
+    name: "Biology",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 26,
+  },
 ];
 const subjectsICSE = [
-	{
-		name: "Mathematics",
-		color: "#FF78A4",
-		color2: "#FF3F7D",
-		secondaryColor: "#FF78A3",
-		color3: "#FF78A320",
-		id: 22,
-	},
-	{
-		name: "Physics",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 24,
-	},
-	{
-		name: "Chemistry",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 23,
-	},
-	{
-		name: "Biology",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 26,
-	},
-	{
-		name: "History Civics",
-		color: "#E543FF",
-		color2: "#EF8CFF",
-		secondaryColor: "#EF90FF",
-		color3: "#FFC46A20",
-		id: 48,
-	},
-	{
-		name: "Geography",
-		color: "#E543FF",
-		color2: "#EF8CFF",
-		secondaryColor: "#EF90FF",
-		color3: "#FFC46A20",
-		id: 20,
-	},
-	{
-		name: "English Language",
-		color: "#ED7685",
-		color2: "#F19DA4",
-		secondaryColor: "#F29DA4",
-		color3: "#F29DA420",
-		id: 45,
-	},
-	{
-		name: "English Literature",
-		color: "#ED7685",
-		color2: "#F19DA4",
-		secondaryColor: "#F29DA4",
-		color3: "#F29DA420",
-		id: 43,
-	},
-	{
-		name: "Hindi",
-		color: "#E543FF",
-		color2: "#EF8CFF",
-		secondaryColor: "#EF90FF",
-		color3: "#FFC46A20",
-		id: 42,
-	},
-	{
-		name: "Commercial Studies",
-		color: "#FFC670",
-		color2: "#FFB23B",
-		secondaryColor: "#FFC46A",
-		color3: "#FFC46A20",
-		id: 47,
-	},
-	{
-		name: "Economic Applications",
-		color: "#ED7685",
-		color2: "#F19DA4",
-		secondaryColor: "#F29DA4",
-		color3: "#F29DA420",
-		id: 46,
-	},
-	{
-		name: "Computer Applications",
-		color: "#E543FF",
-		color2: "#EF8CFF",
-		secondaryColor: "#EF90FF",
-		color3: "#FFC46A20",
-		id: 44,
-	},
+  {
+    name: "Mathematics",
+    color: "#FF78A4",
+    color2: "#FF3F7D",
+    secondaryColor: "#FF78A3",
+    color3: "#FF78A320",
+    id: 22,
+  },
+  {
+    name: "Physics",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 24,
+  },
+  {
+    name: "Chemistry",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 23,
+  },
+  {
+    name: "Biology",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 26,
+  },
+  {
+    name: "History Civics",
+    color: "#E543FF",
+    color2: "#EF8CFF",
+    secondaryColor: "#EF90FF",
+    color3: "#FFC46A20",
+    id: 48,
+  },
+  {
+    name: "Geography",
+    color: "#E543FF",
+    color2: "#EF8CFF",
+    secondaryColor: "#EF90FF",
+    color3: "#FFC46A20",
+    id: 20,
+  },
+  {
+    name: "English Language",
+    color: "#ED7685",
+    color2: "#F19DA4",
+    secondaryColor: "#F29DA4",
+    color3: "#F29DA420",
+    id: 45,
+  },
+  {
+    name: "English Literature",
+    color: "#ED7685",
+    color2: "#F19DA4",
+    secondaryColor: "#F29DA4",
+    color3: "#F29DA420",
+    id: 43,
+  },
+  {
+    name: "Hindi",
+    color: "#E543FF",
+    color2: "#EF8CFF",
+    secondaryColor: "#EF90FF",
+    color3: "#FFC46A20",
+    id: 42,
+  },
+  {
+    name: "Commercial Studies",
+    color: "#FFC670",
+    color2: "#FFB23B",
+    secondaryColor: "#FFC46A",
+    color3: "#FFC46A20",
+    id: 47,
+  },
+  {
+    name: "Economic Applications",
+    color: "#ED7685",
+    color2: "#F19DA4",
+    secondaryColor: "#F29DA4",
+    color3: "#F29DA420",
+    id: 46,
+  },
+  {
+    name: "Computer Applications",
+    color: "#E543FF",
+    color2: "#EF8CFF",
+    secondaryColor: "#EF90FF",
+    color3: "#FFC46A20",
+    id: 44,
+  },
+  {
+    name: "EVS",
+    color: "#E543FF",
+    color2: "#EF8CFF",
+    secondaryColor: "#EF90FF",
+    color3: "#FFC46A20",
+    id: 49,
+  },
 ];
--- src/views/Practice/PracticeBySubject/PracticeBySubject.js
+++ src/views/Practice/PracticeBySubject/PracticeBySubject.js
@@ -5,6 +5,7 @@ import { ReactComponent as KannadaIcon } from "../../../assets/subjects/Kannada.
 import { ReactComponent as MathIcon } from "../../../assets/subjects/MathIcon.svg";
 import { ReactComponent as ScienceIcon } from "../../../assets/subjects/ScienceIcon.svg";
 import { ReactComponent as SocialSciencesIcon } from "../../../assets/subjects/SocialSciences.svg";
+import { ReactComponent as EVSIcon } from "../../../assets/subjects/SocialSciences.svg";
 
 import { ReactComponent as ArrowRightCircleIcon } from "../../../assets/subjects/ArrowRightCircle.svg";
 import { ReactComponent as ComputerApplicationsIcon } from "../../../assets/subjects/ComputerApplications.svg";
@@ -18,102 +19,104 @@ import { fetchSubjects } from "store/actions/practiceBySubjectActions";
 import CustomSpinner from "components/CustomSpinner";
 // import getColorById from "components/helpers/getColorById";
 const PracticeBySubject = (props) => {
-	const subjects = useSelector((state) => Object.values(state.subjects));
-	const history = useHistory();
-	const location = useLocation();
-	const dispatch = useDispatch();
+  const subjects = useSelector((state) => Object.values(state.subjects));
+  const history = useHistory();
+  const location = useLocation();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(fetchSubjects());
-	}, []);
+  useEffect(() => {
+    dispatch(fetchSubjects());
+  }, []);
 
-	const getIconById = (id) => {
-		switch (id) {
-			case 22:
-				return <MathIcon />;
-			case 23:
-			case 24:
-			case 25:
-			case 26:
-				return <ScienceIcon />;
-			case 33:
-			case 42:
-				return <HindiIcon />;
-			case 20:
-			case 35:
-			case 48:
-			case 46:
-			case 47:
-				return <SocialSciencesIcon />;
-			case 36:
-			case 37:
-			case 38:
-			case 41:
-			case 43:
-			case 45:
-				return <EnglishIcon />;
-			case 34:
-				return <KannadaIcon />;
-			case 44:
-				return <ComputerApplicationsIcon />;
-			default:
-				// console.log(id);
-				return <KannadaIcon />;
-		}
-	};
-	// const getIconById = (id) => {
-	// 	switch (id) {
-	// 		case 22:
-	// 			return <MathIcon />;
-	// 		case 25:
-	// 			return <ScienceIcon />;
-	// 		case 33:
-	// 			return <HindiIcon />;
-	// 		case 35:
-	// 			return <SocialSciencesIcon />;
-	// 		case 36:
-	// 		case 37:
-	// 		case 38:
-	// 			return <EnglishIcon />;
-	// 		default:
-	// 			console.log(id);
-	// 			return <KannadaIcon />;
-	// 	}
-	// };
+  const getIconById = (id) => {
+    switch (id) {
+      case 22:
+        return <MathIcon />;
+      case 23:
+      case 24:
+      case 25:
+      case 26:
+        return <ScienceIcon />;
+      case 33:
+      case 42:
+        return <HindiIcon />;
+      case 20:
+      case 35:
+      case 48:
+      case 46:
+      case 47:
+        return <SocialSciencesIcon />;
+      case 36:
+      case 37:
+      case 38:
+      case 41:
+      case 43:
+      case 45:
+        return <EnglishIcon />;
+      case 34:
+        return <KannadaIcon />;
+      case 44:
+        return <ComputerApplicationsIcon />;
+      case 49:
+        return <SocialSciencesIcon />;
+      default:
+        // console.log(id);
+        return <KannadaIcon />;
+    }
+  };
+  // const getIconById = (id) => {
+  // 	switch (id) {
+  // 		case 22:
+  // 			return <MathIcon />;
+  // 		case 25:
+  // 			return <ScienceIcon />;
+  // 		case 33:
+  // 			return <HindiIcon />;
+  // 		case 35:
+  // 			return <SocialSciencesIcon />;
+  // 		case 36:
+  // 		case 37:
+  // 		case 38:
+  // 			return <EnglishIcon />;
+  // 		default:
+  // 			console.log(id);
+  // 			return <KannadaIcon />;
+  // 	}
+  // };
 
-	const listBlock = () => {
-		return (
-			subjects &&
-			subjects.map((subject) => {
-				return (
-					<Link
-						className={style.subjectsBlock}
-						key={subject.id}
-						to={`${location.pathname}/${subject.id}`}
-					>
-						{getIconById(subject.id)}
-						<span className={style.subjectName}>{subject.name}</span>
-						<button className={style.subjectArrow}>
-							<ArrowRightCircleIcon
-								style={{ height: "4.5rem", width: "4.5rem" }}
-							/>
-						</button>
-					</Link>
-				);
-			})
-		);
-	};
+  const listBlock = () => {
+    return (
+      subjects &&
+      subjects.map((subject) => {
+        return (
+          <Link
+            className={style.subjectsBlock}
+            key={subject.id}
+            to={`${location.pathname}/${subject.id}`}
+          >
+            {getIconById(subject.id)}
+            <span className={style.subjectName}>{subject.name}</span>
+            <button className={style.subjectArrow}>
+              <ArrowRightCircleIcon
+                style={{ height: "4.5rem", width: "4.5rem" }}
+              />
+            </button>
+          </Link>
+        );
+      })
+    );
+  };
 
-	return (
-		<div className="w-100">
-			<h2 className={`${style.h2} primary`}>Practice by Subject</h2>
-			{subjects.length > 0 ? (
-				<div className={style.subjects}> {listBlock()}</div>
-			) : (
-				<CustomSpinner height="100%" />
-			)}
-		</div>
-	);
+  return (
+    <div className="w-100">
+      <h2 className={`${style.h2} primary`}>Practice by Subject</h2>
+      {subjects.length > 0 ? (
+        <div className={style.subjects}> {listBlock()}</div>
+      ) : (
+        <CustomSpinner height="100%" />
+      )}
+    </div>
+  );
 };
 
 export default PracticeBySubject;
